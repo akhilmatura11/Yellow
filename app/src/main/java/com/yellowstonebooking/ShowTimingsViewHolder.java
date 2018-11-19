b4ed5c6094e5a79a052d766ff2863c268e80ccb4
Akhil Matura <akhil.matura@gmail.com>
2018-11-19 23:32:01 +0530

    5th Commit
    Signed-off-by: Akhil Matura <akhil.matura@gmail.com>

--- app/src/main/java/com/yellowstonebooking/ShowTimingsViewHolder.java
+++ app/src/main/java/com/yellowstonebooking/ShowTimingsViewHolder.java
@@ -2,25 +2,27 @@ package com.yellowstonebooking;
 
 import android.content.Context;
 import android.content.Intent;
+import android.support.v7.widget.CardView;
 import android.support.v7.widget.RecyclerView;
 import android.view.View;
 import android.widget.TextView;
-import android.widget.Toast;
 
 public class ShowTimingsViewHolder extends RecyclerView.ViewHolder {
     private Context mContext;
     private TextView mShowTimings;
+    private CardView mCardView;
 
     public ShowTimingsViewHolder(View itemView, Context context) {
         super(itemView);
         mContext = context;
         mShowTimings = itemView.findViewById(R.id.showTimingsTextView);
+        mCardView = itemView.findViewById(R.id.showTimingCardView);
     }
 
 
     public void setRowDetails(String s) {
         mShowTimings.setText(s);
-        mShowTimings.setOnClickListener(new View.OnClickListener() {
+        mCardView.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
                 Intent intent = new Intent(mContext, SeatSelection.class);
